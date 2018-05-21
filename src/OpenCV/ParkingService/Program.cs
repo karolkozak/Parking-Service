752e49203832a1c7553163a326ab217fd3ad6cd4
Jakub Piekarz <jpiekarz12@outlook.com>
2018-05-21 22:58:44 +0200

    Add license plate detector

--- src/OpenCV/ParkingService/Program.cs
+++ src/OpenCV/ParkingService/Program.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using ParkingService.Services;
 
 namespace ParkingService
 {
@@ -11,7 +10,30 @@ namespace ParkingService
     {
         static void Main(string[] args)
         {
-            
+            LicensePlateDetector licensePlateDetector = new LicensePlateDetector("");
+            Mat m = new Mat("license-plate.jpg");
+            //UMat um = m.GetUMat(AccessType.ReadWrite);
+            var words = ProcessImage(m, licensePlateDetector);
+
+            foreach (var word in words)
+            {
+                Console.WriteLine(word);
+            }
+
+            Console.ReadKey();
+        }
+
+        private static List<string> ProcessImage(IInputOutputArray image, LicensePlateDetector licensePlateDetector)
+        {
+
+            List<IInputOutputArray> licensePlateImagesList = new List<IInputOutputArray>();
+            List<IInputOutputArray> filteredLicensePlateImagesList = new List<IInputOutputArray>();
+            List<RotatedRect> licenseBoxList = new List<RotatedRect>();
+            return licensePlateDetector.DetectLicensePlate(
+                image,
+                licensePlateImagesList,
+                filteredLicensePlateImagesList,
+                licenseBoxList);
         }
     }
 }
