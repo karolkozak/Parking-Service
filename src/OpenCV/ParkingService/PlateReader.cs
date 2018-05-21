82afc157b6386aaa6642c1a1738e7984773095e5
Ryszard Sikora <ryszard.sikora7@gmail.com>
2018-05-22 00:32:08 +0200

    Added queue

---
+++ src/OpenCV/ParkingService/PlateReader.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using ParkingService.Models;
+
+namespace ParkingService
+{
+    public class PlateReader
+    {
+        public async static Task ProcessPhotos([ServiceBusTrigger("plates"/*, Connection = "ServiceBus"*/)]
+            string request, ILogger logger)
+        {
+            logger.LogInformation("Successfully finished " + request);
+        }
+    }
+}
--- src/OpenCV/ParkingService/Program.cs
+++ src/OpenCV/ParkingService/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
 
 namespace ParkingService
 {
@@ -11,7 +13,32 @@ namespace ParkingService
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("WebJob started");
+
+            JobHostConfiguration config = new JobHostConfiguration();
+            ConfigureJobHost(config);
+            JobHost host = new JobHost(config);
             
+            var cancellationToken = new WebJobsShutdownWatcher().Token;
+            cancellationToken.Register(() =>
+            {
+                // Handle any shutdown operations here
+                config.LoggerFactory.Dispose();
+                host.Stop();
+            });
+
+
+            host.RunAndBlock();
+            Console.WriteLine("WebJob stopped");
+        }
+
+        private static void ConfigureJobHost(JobHostConfiguration config)
+        {
+            var log = new LoggerFactory();
+            config.UseServiceBus();
+            config.DashboardConnectionString = "";
+            var loggerFactory = log.AddConsole();
+            config.LoggerFactory = loggerFactory;
         }
     }
 }
